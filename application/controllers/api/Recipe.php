bf03a7d734ae14c64eccdf5f8b1ec4dd9d38080f
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-14 16:44:28 -0400

    Added post new recipe api endpoint.

--- application/controllers/api/Recipe.php
+++ application/controllers/api/Recipe.php
@@ -1,5 +1,18 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Recipe extends CI_Controller {    
-    
+    public function new() {
+        $data = verifyRequest();
+		if(!$data) {
+			return authenticationFailedRequest();
+		}
+
+		$this->load->model('Recipe_model');
+        $result = $this->Recipe_model->new($data['user_id']);
+        if($result) {    
+            return responseWithHeader(true, $result);    		
+        } else {
+            return responseWithHeader(true, []);    		
+        }
+    }
 }
