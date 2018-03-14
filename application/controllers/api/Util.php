b029f8d6d7dc9c9a6cb9220937ca3a187a44e455
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-13 23:37:36 -0400

    Added upload api endpoint and implemented file upload.

--- application/controllers/api/Util.php
+++ application/controllers/api/Util.php
@@ -15,4 +15,21 @@ class Util extends CI_Controller {
             return responseWithHeader(true, []);    		
         }
     }
+    public function upload() {                        
+        $token = JWT::decode($_POST['token'], SECRET_SERVER_KEY);
+        $userid = $token->id;
+        $total_files_uploaded = array();    
+        $countfiles = count($_FILES['file']['name']);
+
+        for($i = 0; $i < $countfiles; $i++) {
+            $filename = $_FILES['file']['name'][$i];                                                        
+            $extension = pathinfo($filename, PATHINFO_EXTENSION);
+            $temp = explode(".", $_FILES["file"]["name"][$i]);
+            $newfilename = rand(1, 999999) . '' . time() . '.' . end($temp);
+
+            move_uploaded_file($_FILES['file']['tmp_name'][$i], 'userdata/' . $userid . '/' . $newfilename);
+            array_push($total_files_uploaded, 'userdata/' . $userid . '/' . $newfilename);
+        }
+        echo json_encode(array('success'=>true, 'files'=>$total_files_uploaded));
+    }
 }
