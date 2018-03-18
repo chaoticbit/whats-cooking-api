e1589b3cff34f38ae5b3dce31ac4467824b7b8b8
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-17 20:11:08 -0400

    minor fixes in recipe model.

--- application/models/Recipe_model.php
+++ application/models/Recipe_model.php
@@ -6,8 +6,11 @@ class Recipe_model extends CI_Model {
 
         $query = $this->db->query("select recipes.srno as recipe_id, recipes.title, recipes.description, recipes.cover_imagepath, recipes.prep_time, recipes.cooking_time, recipes.servings, recipes.spicy, recipes.food_group, recipes.cid, recipes.uid, recipes.timestamp, CONCAT(useraccounts.fname,' ',useraccounts.lname) as fullname, cuisines.name as cname FROM recipes, useraccounts, cuisines where recipes.uid = " . (int)$data['user_id']. " AND recipes.uid=useraccounts.srno AND recipes.cid=cuisines.srno order by timestamp DESC LIMIT 1");
         if($query->num_rows() > 0) {
-            $result = $query->row_array();            
-            $result['cover_imagepath'] = 'userdata/' . $result['user_id'] . '/' . $result['cover_imagepath'];
+            $result = $query->row_array();    
+            if($result['cover_imagepath'] != '')
+                $result['cover_imagepath'] = 'userdata/' . $data['user_id'] . '/' . $result['cover_imagepath'];
+            else 
+                $result['cover_imagepath'] = '';
             $result['rating'] = 0;
             $result['spicy'] = (int)$result['spicy'];
 
@@ -53,7 +56,10 @@ class Recipe_model extends CI_Model {
         if($query->num_rows() > 0) { 
             $result = $query->result_array();      
             for($i=0;$i<count($result);$i++) {
-                $result[$i]['cover_imagepath'] = 'userdata/' . (int)$result[$i]['uid'] . '/' . $result[$i]['cover_imagepath'];
+                if($result[$i]['cover_imagepath'] != '') 
+                    $result[$i]['cover_imagepath'] = 'userdata/' . (int)$result[$i]['uid'] . '/' . $result[$i]['cover_imagepath'];
+                else 
+                $result[$i]['cover_imagepath'] = '';                
                 $query_ = $this->db->query("select * from favourites where rid = " . (int)$result[$i]['recipe_id'] . " and uid = " . $user_id . "");                
                 if($query_->num_rows() > 0) {
                     $result[$i]['addedToFavourites'] = true;
