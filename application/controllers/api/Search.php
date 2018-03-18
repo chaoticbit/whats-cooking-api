637522d36fa9019b0da6f75f0a5985eda5da5129
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-18 16:44:32 -0400

    Implemented quick search recipes.

--- application/controllers/api/Search.php
+++ application/controllers/api/Search.php
@@ -1,5 +1,18 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Search extends CI_Controller {    
-    
+    public function quick($key) {
+        $data = verifyRequest();
+        if(!$data) {
+			return authenticationFailedRequest();
+        }
+        
+        $this->load->model('Search_model');
+        $result = $this->Search_model->quickSearch($data['user_id'], $key);
+        if($result) {
+            return responseWithHeader(true, $result);    		        
+        } else {
+            return responseWithHeader(true, []);    		        
+        }
+    }
 }
--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -1,5 +1,12 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Search_model extends CI_Model { 
-
+    public function quickSearch($srno, $key) {
+        $response = array();
+        $query = $this->db->query("SELECT recipes.srno, recipes.uid, recipes.title, recipes.cover_imagepath, ratings.rating, CONCAT(useraccounts.fname, ' ', useraccounts.lname) as name FROM recipes, useraccounts, ratings WHERE (recipes.title LIKE '%" . $key . "%' OR recipes.ingredients LIKE '%" . $key . "%') AND recipes.uid=useraccounts.srno AND recipes.srno = ratings.rid LIMIT 15");        
+        if($query->num_rows() > 0) { 
+            return $query->result_array();
+        }
+        return false;
+    }
 }
