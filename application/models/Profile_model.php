939c3ac435ba96a565e7efa440fe44a7eaa99d13
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-24 09:21:18 -0400

    Added minor attributes in get queries.

--- application/models/Profile_model.php
+++ application/models/Profile_model.php
@@ -7,10 +7,20 @@ class Profile_model extends CI_Model {
         $validate = $this->db->query("select * from useraccounts where username = '$username'");
         if($validate->num_rows() > 0) {            
             $row = $validate->row_array();
-            $query = $this->db->query("select useraccounts.srno, useraccounts.username, concat(useraccounts.fname, ' ', useraccounts.lname) as fullname, useraccounts.email, userprofile.* from useraccounts, userprofile where useraccounts.srno = " . $row['srno'] . " and userprofile.uid = useraccounts.srno");              
+            $query = $this->db->query("select useraccounts.srno, useraccounts.username, concat(useraccounts.fname, ' ', useraccounts.lname) as fullname, useraccounts.email, userprofile.*, cuisines.name as cname from useraccounts, userprofile, cuisines where userprofile.pref_cuisine = cuisines.srno and useraccounts.srno = " . $row['srno'] . " and userprofile.uid = useraccounts.srno");              
             $result = $query->row_array();
             
-            $top_rated = $this->db->query("select recipes.srno, recipes.title from recipes, ratings where recipes.srno = ratings.rid order by rating DESC");
+            $top_rated = $this->db->query("select recipes.srno as recipe_id, recipes.title, concat(useraccounts.fname, ' ', useraccounts.lname) as fullname, useraccounts.username, ratings.rating from recipes, ratings, useraccounts where recipes.uid = " . $result['srno'] . " and recipes.uid = useraccounts.srno and recipes.srno = ratings.rid order by rating DESC");
+            if($top_rated->num_rows() > 0) {
+                $result['top_rated_recipes'] = $top_rated->result_array();
+            } else {
+                $result['top_rated_recipes'] = [];
+            }
+
+            $recipe_count = $this->db->query("SELECT * FROM recipes where uid = " . $result['srno'] . "");
+            $result['recipe_count'] = $recipe_count->num_rows();            
+
+            return $result;
         }
         return false;
     }
