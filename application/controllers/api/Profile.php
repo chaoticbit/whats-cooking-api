59c64bdbda83dc8581ce2047b92837ca12a72a46
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-03-23 23:24:10 -0400

    EOD commit.

--- application/controllers/api/Profile.php
+++ application/controllers/api/Profile.php
@@ -3,17 +3,28 @@
 class Profile extends CI_Controller {    
    
 	public function userprofile() {
-		$data = verifyRequest();
-		if(!$data) {
-			return authenticationFailedRequest();
-		}
+        
+        $apikey = $this->input->get_request_header('x-api-key');    
+        if(empty($apikey) || !$apikey || $apikey == '') { //Check API KEY Existence
+            echo 'api key empty';
+            return false;
+        }
+        if($apikey != '') { 
+            if($apikey != API_KEY) {                        
+                return false;
+            }             
+        } else {
+            return false;
+        }
+        
+        $data = processRequest();
 
 		$this->load->model('Profile_model');
         $result = $this->Profile_model->getUserProfile($data);
         if($result) {    
             return responseWithHeader(true, $result);    		
         } else {
-            return responseWithHeader(true, []);    		
+            return responseWithHeader(false, false);    		
         }
     }
 }
--- application/models/Profile_model.php
+++ application/models/Profile_model.php
@@ -2,6 +2,16 @@
 
 class Profile_model extends CI_Model {   
     public function getUserProfile($data) {
-        
+        $username = $data['username'];
+
+        $validate = $this->db->query("select * from useraccounts where username = '$username'");
+        if($validate->num_rows() > 0) {            
+            $row = $validate->row_array();
+            $query = $this->db->query("select useraccounts.srno, useraccounts.username, concat(useraccounts.fname, ' ', useraccounts.lname) as fullname, useraccounts.email, userprofile.* from useraccounts, userprofile where useraccounts.srno = " . $row['srno'] . " and userprofile.uid = useraccounts.srno");              
+            $result = $query->row_array();
+            
+            $top_rated = $this->db->query("select recipes.srno, recipes.title from recipes, ratings where recipes.srno = ratings.rid order by rating DESC");
+        }
+        return false;
     }
 }
