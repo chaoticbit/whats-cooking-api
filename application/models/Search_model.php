3c042fadeb8c9211c707e497c94035e52c0b5643
Atharva Dandekar <dandekar.atharva@gmail.com>
2018-04-05 00:20:50 -0400

    Minor changes in search ingredients api.

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -10,60 +10,64 @@ class Search_model extends CI_Model {
         return false;
     }
 
+    public function bubble_sort($arr) {
+        $size = count($arr)-1;
+        for ($i=0; $i<$size; $i++) {
+            for ($j=0; $j<$size-$i; $j++) {
+                $k = $j+1;
+                if ($arr[$k]['cosine'] > $arr[$j]['cosine']) {
+                    // Swap elements at indices: $j, $k
+                    list($arr[$j], $arr[$k]) = array($arr[$k], $arr[$j]);
+                }
+            }
+        }
+        return $arr;
+    }
+
     public function ingredientSearch($keyword, $userid, $ingredients) {
-        // $query = $this->db->query("SELECT recipes.srno, recipes.ingredients from recipes, userprofile where title LIKE '%" . $keyword . "%' AND userprofile.uid = " . $userid . " AND recipes.food_group <= userprofile.food_group AND recipes.spicy <= userprofile.spiciness");
-        // $result = $query->result_array();
+        /* $query = $this->db->query("SELECT distinct recipes.srno, recipes.title, recipes.ingredients from recipes, userprofile where title LIKE '%" . $keyword . "%'");
+        $result = $query->result_array();
 
-        // $articles = array();
-        // for($i = 0; $i < count($result); $i++) {
-        //     $articles[$i] = explode(',',$result[$i]['ingredients']);
-        //     $articles[$i]['srno'] = $result[$i]['srno'];
-        // }
+        $articles = array();
+        for($i = 0; $i < count($result); $i++) {
+            $articles[$i] = explode(',',$result[$i]['ingredients']);
+            $articles[$i]['title'] = $result[$i]['title'];
+        }
         
-        // $similarity = new Similarity();
-        // $tags = $similarity::tags_to_point($articles);
-        // $target = explode(',', $ingredients);
-        // $compare = array_fill_keys($target, 1) + $tags;
+        $similarity = new Similarity();
+        $tags = $similarity::tags_to_point($articles);
+        $target = explode(',', $ingredients);
+        $compare = array_fill_keys($target, 1) + $tags;
 
-        // $resp = array();        
-        // foreach($articles as $article) {
-        //     $ak = array_fill_keys($article, 1) + $tags;
-        //     echo $article['srno'];
-        //     array_push($resp, implode(",", $article) . ' ' . $similarity::cosine($compare, $ak));                        
-        // }
-        // die(print_r($resp));
-        // return $resp;
+        $resp = array();        
+        foreach($articles as $article) {
+            $ak = array_fill_keys($article, 1) + $tags;              
+            array_push($resp, array(
+                'title'=>$article['title'],
+                'ingredients'=>implode(',', $article),
+                'cosine'=>$similarity::cosine($compare, $ak)
+            ));            
+        }          
+        $resp = $this->bubble_sort($resp);
+        header('Content-Type: application/json');
+        die(json_encode($resp));         */
 
-        $ing = explode(',', $ingredients);        
-        $orig = $this->db->db_debug;
-        $this->db->db_debug = FALSE;
-        
+        $ing = explode(',', $ingredients);                
         $response = array();
 
-        $query = $this->db->query("select recipes.srno, recipes.ingredients from recipes where FIND_IN_SET('" . $ing[0] . "', ingredients) AND FIND_IN_SET('" . $ing[1] . "', ingredients) AND FIND_IN_SET('" . $ing[2] . "', ingredients)");
-        $exact_match_results = $query->result_array();        
-        array_push($response, $exact_match_results);
+        // $query = $this->db->query("select recipes.srno, recipes.title, recipes.ingredients from recipes where FIND_IN_SET('" . $ing[0] . "', ingredients) AND FIND_IN_SET('" . $ing[1] . "', ingredients) AND FIND_IN_SET('" . $ing[2] . "', ingredients)");
+        // $exact_match_results = $query->result_array();        
+        // array_push($response, $exact_match_results);
 
-        $query2 = $this->db->query("select recipes.srno, recipes.ingredients from recipes where FIND_IN_SET('" . $ing[0] . "', ingredients) OR FIND_IN_SET('" . $ing[1] . "', ingredients) OR FIND_IN_SET('" . $ing[2] . "', ingredients)");
-        $result2 = $query2->result_array();        
-        array_push($response, $result2);
-
-        $this->db->db_debug = $orig;
-        
-        // $no_duplicate = [];
-        
-        // if($query->num_rows() > 0) {
-        //     for ($i=0; $i < count($exact_match_results); $i++) { 
-        //         for ($j=0; $j < count($result2); $j++) { 
-        //             if($exact_match_results[$i]['srno'] != $result2[$j]['srno']) {
-        //                 array_push($no_duplicate, $result2[$j]);
-        //             }         
-        //         }    
-        //     }            
-        // } else {
-        //     // array_push($response, $result2);
-        // }        
+        // $query2 = $this->db->query("select recipes.srno, recipes.title, recipes.ingredients from recipes where FIND_IN_SET('" . $ing[0] . "', ingredients) OR FIND_IN_SET('" . $ing[1] . "', ingredients) OR FIND_IN_SET('" . $ing[2] . "', ingredients)");
+        // $result2 = $query2->result_array();        
+        // array_push($response, $result2);        
                 
-        die(json_encode($response));
+        $query2 = $this->db->query("select recipes.srno, recipes.title, recipes.ingredients from recipes where MATCH (ingredients) AGAINST ('" . $ing[0] . "' IN NATURAL LANGUAGE MODE)");
+        $result2 = $query2->result_array();        
+        // array_push($response, $result2);        
+
+        header('Content-Type: application/json');
+        die(json_encode($result2));
     }
 }
